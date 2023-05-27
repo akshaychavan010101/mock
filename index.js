59fa57048a2257f48da8e9bbe3f102c9b73015fc
Akshay Chavan <115460540+akshaychavan010101@users.noreply.github.com>
2023-05-27 16:33:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const fs = require('@cyclic.sh/s3fs') ;
+const fs = require('fs') ;
 
 const app = express();
 
